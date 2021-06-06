0845156ce5fb3917d578591f6e3fe0f100f29422
Alin Ursu <33602125+ursualin7890@users.noreply.github.com>
2021-06-06 16:52:44 +0300

    Added functionality to import users and gamification systems, events and rewards from CSV.

--- gamification/backend/pages/js/admin/gamificationSystems.js
+++ gamification/backend/pages/js/admin/gamificationSystems.js
@@ -9,3 +9,18 @@ const deleteSystem = (api_key) => {
 const updateSystem = (api_key) => {
     location.href = '/admin/gamification-systems/update?api_key=' + encodeURIComponent(api_key);
 }
+
+const importBtn = () => {
+    document.getElementById('imported-CSV-file').click()
+}
+
+const handleImportFilesChange = () => {
+    const node = document.getElementById('imported-CSV-file');
+    if(node.files.length > 0) {
+        const actionConfirm = confirm("Do you want to import data from file \""+ node.files[0].name + "\"?");
+        if(actionConfirm) {
+            document.getElementById('import-CSV-submit').click();
+        }
+    }
+}
+document.getElementById('imported-CSV-file').addEventListener('change', handleImportFilesChange);
